3c571e131c9580b2e66700e5e25ea00c3f0b9c89
Jesse Huang <jessejhuang@gmail.com>
2018-09-27 08:43:25 -0500

    zhengliang's code

--- README.md
+++ README.md
@@ -5,8 +5,9 @@
 - Zhengliang Liu, zhengliang@wustl.edu
 
 ## Module Design
+
 We built lab01_module.c with the simple module example on p338 and p339 of LKD,
 
-
 ## Timer Design and Evaluation
+
 explanation here
--- mt_lab01_module.c
+++ mt_lab01_module.c
@@ -13,6 +13,9 @@ MODULE_AUTHOR("Elaine Cole, Jesse Huang, Zhengliang Liu");
 MODULE_DESCRIPTION("TIMER MODULE");
 
 static struct task_struct *task;
+static struct task_struct *task2;
+static struct task_struct *task3;
+static struct task_struct *task4;
 static ulong log_sec = 1;
 static ulong log_nsec = 0; // What to put here? Is the time sec + nsec?
 module_param(log_sec, ulong, 0);
@@ -29,7 +32,7 @@ counter = 0;
 while(!kthread_should_stop())
 {
 //struct task_struct *temp_curr = current; //global macro
-printk("labfunction is still running with itr %d, nvcsw %lu, nivcsw %lu\n", counter, current->nvcsw, current->nivcsw);
+printk("labfunction is still running with processor %d,  itr %d, nvcsw %lu, nivcsw %lu\n",smp_processor_id(), counter, current->nvcsw, current->nivcsw);
 ++counter;
 
 set_current_state(TASK_INTERRUPTIBLE);
@@ -43,6 +46,9 @@ enum hrtimer_restart nextcall(struct hrtimer *some_timer)
 {
 printk("timer is working");
 wake_up_process(task);
+wake_up_process(task2);
+wake_up_process(task3);
+wake_up_process(task4);
 hrtimer_forward_now(some_timer, interval);
 return HRTIMER_RESTART;
 }
@@ -55,7 +61,17 @@ static int __init lab01_module_init(void)
 interval = ktime_set(log_sec, log_nsec);
 hrtimer_init(&timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 timer.function = &nextcall;
-task = kthread_run(labfunction,NULL,"mythread");
+//task = kthread_run(labfunction,NULL,"mythread");
+task = kthread_create(labfunction,NULL,"mythread");
+task2 = kthread_create(labfunction,NULL,"mythread2");
+task3 = kthread_create(labfunction,NULL,"mythread3");
+task4 = kthread_create(labfunction, NULL,"mythread4");
+
+kthread_bind(task,0);
+kthread_bind(task2,1);
+kthread_bind(task3,2);
+kthread_bind(task4,3);
+
 hrtimer_start(&timer, interval,HRTIMER_MODE_REL); 
 return 0;
 }
@@ -64,6 +80,9 @@ static void __exit lab01_module_exit(void)
 {
 printk("exiting");
 kthread_stop(task);
+kthread_stop(task2);
+kthread_stop(task3);
+kthread_stop(task4);
 hrtimer_cancel(&timer);
 
 }
